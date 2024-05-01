a2a3a3f6b0055b9c507f183edc9e66145f7e40ab
ArifAlakbarovv <116842869+ArifAlakbarovv@users.noreply.github.com>
2024-05-01 16:52:32 +0400

    Create _start.c

---
+++ _start.c
@@ -0,0 +1,46 @@
+
+#include "main.h"
+/**
+ * _start_ - check mode, generate prompt, Ctrl D and buffer with arguments
+ * @flag: flag
+ * Return: Buffer string with arguments
+ */
+
+char *_start_(int flag)
+{
+	char *buffer = NULL;
+	size_t buffsize = 0;
+	int i;
+
+	/*Evaluo modo interactivo*/
+	if (isatty(0) == 1) /*0 el fd de stdin vale 0*/
+		printf("$ ");
+	if (getline(&buffer, &buffsize, stdin) == -1)
+	{
+		free(buffer);
+		exit(0);
+	}
+	if (buffer[0] == '\n')
+	{
+		free(buffer);
+		return (NULL);
+	}
+	buffer = strtok(buffer, "\n");
+
+	for (i = 0; buffer[i]; i++) /* recorre la string en busca de ' ' o '\t'*/
+	{
+		if (buffer[i] != ' ' && buffer[i] != '\t')
+			break;
+	}
+	if (buffer[i] == '\0') /*significa que recorrio hasta el final*/
+	{
+		free(buffer);
+		return (NULL);
+	}
+	if (strstr(buffer, "exit"))
+	{
+		free(buffer);
+		exit(flag);
+	}
+	return (buffer);
+}
